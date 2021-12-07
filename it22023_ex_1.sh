38c80a2bc2c7ca0d4ab41ff4defd2758d810ca90
Ledrake <nikosgournak@gmail.com>
2021-12-07 18:30:16 +0200

    Added random.img 's

--- it22023_ex_1.sh
+++ it22023_ex_1.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-echo "Hello World"
+
+#Change Directory into /home/user
+cd ~
+
+#Variable file because it is used in multiple places
+file=".hd_size"
+
+#create the file
+touch $file
+
+#append the date into the file
+date >> $file
+
+#append the size of the home/user into the file
+#use flags -h to make the command display the size in KB/MB/GB.
+#          -s to only show the home/user dir instead of every nested dir/file
+du -hs ~ >> $file
+
+#put separation line
+echo "************" >> $file
