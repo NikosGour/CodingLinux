44971ca05a64a62c2ac9b068be58bfda562142ab
Ledrake <nikosgournak@gmail.com>
2021-12-11 07:41:35 +0200

    ex3

---
+++ it22023_ex_3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Calculating the size of root and home by:
+#       1) Getting the dir size from du 
+#       2) Using awk to get only the size and not the file path
+#       3) Doing a routine to remove the size type (G,M,K)
+#       3.1) Reversing the number plus type (18G -> G81)
+#       3.2) Cutting all the characters from the second character (G81 -> 81)
+#       3.3) Reversing the string again to gain the number (81 -> 18) 
+root_sz=$(du -sh / 2> /dev/null | awk '{print $1}' | rev | cut -c 2- | rev)
+home_sz=$(du -sh $HOME 2> /dev/null | awk '{print $1}'| rev | cut -c 2- | rev)
+
+#Calulating the percentage of home to root by:
+#       1) Using bc to calculate "home_sz / root_sz * 100" (4.1 / 18 * 100 = 22.777777)
+#       2) Cutting all the characters from the dot
+percentage_of_home=$(echo "$home_sz / $root_sz * 100" | bc -l | cut -c -2) 
+
+
+echo "Root filesystem size: ${root_sz}G"
+echo "Home directory size: ${home_sz}G"
+echo "Home directory uses ${percentage_of_home}% of /"
